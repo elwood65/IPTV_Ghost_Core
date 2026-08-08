b42561ef953590eee0555adc031c8b60c63d2d27
giampy2 <58866683+giampy2@users.noreply.github.com>
2026-08-08 17:04:03 +0200

    Remove one source URL from SOURCES list

--- update_playlist.py
+++ update_playlist.py
@@ -8,8 +8,7 @@ HEADERS = {
 }
 
 SOURCES = [
-    'http://vitftopuptop.xubi.org:25461/get.php?username=Pluto&password=m3WxRfR&type=m3u_plus&output=ts',
-    'http://bl4ck.loseyourip.com/get.php?username=PaperinoPaperino1&password=xKGU0Bk9kP&type=m3u_plus&output=mpegts'
+    'http://vitftopuptop.xubi.org:25461/get.php?username=Pluto&password=m3WxRfR&type=m3u_plus&output=ts'
 ]
 
 def main():
