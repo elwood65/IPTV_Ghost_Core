5d2f09769ba2bf1ba7591d66d594f4cf3a61b949
giampy2 <58866683+giampy2@users.noreply.github.com>
2026-08-23 09:54:21 +0200

    Change output format from m3u8 to m3u

--- update_playlist.py
+++ update_playlist.py
@@ -13,7 +13,7 @@ HEADERS = {
 SOURCES = [
     "http://vitftopuptop.xubi.org:25461/"
     "get.php?username=Pluto&password=m3WxRfR"
-    "&type=m3u_plus&output=m3u8"
+    "&type=m3u_plus&output=m3u"
 ]
 
 OUTPUT_FILE = "playlist.m3u"
